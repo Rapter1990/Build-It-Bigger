1479314207ab7e58c1fd40de245b0b3958a35b77
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-04-30 01:14:31 +0300

    commit changes

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -0,0 +1,69 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.AsyncTask;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+// TODO 37 ) Creating EndpointsAsyncTask to retrieve jokes via AsyncTask
+public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+
+    // TODO 38 ) Defining MyApi and EndpointsTaskListener
+    private static MyApi myApiService = null;
+    private EndpointsTaskListener mListener = null;
+
+    // TODO 39 ) Creating EndpointsTaskListener interface to check whether jokes are displayed or not via onComplete
+    public static interface EndpointsTaskListener {
+        public void onComplete(String joke);
+    }
+
+    // TODO 40 ) Implementing Google Client Request Initializer in doInBackground
+    @Override
+    protected String doInBackground(Void... voids) {
+        if (myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        // TODO 41 ) Executing sayJoke to get jokes from CreatingJoke from providejokes module
+        try {
+            return myApiService.sayJoke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    // TODO 42 ) Checking whether joke is listed by opening an Intent or not.
+    @Override
+    protected void onPostExecute(String s) {
+        this.mListener.onComplete(s);
+    }
+
+
+    // TODO 47 ) Setting EndpointsTaskListener and Returning it
+    public EndpointsAsyncTask setListener(EndpointsTaskListener listener) {
+        this.mListener = listener;
+        return this;
+    }
+}
+
+
+
