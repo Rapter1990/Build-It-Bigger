74f2a6ad8b90821ec9ca981762821614c374b315
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-05-01 02:39:18 +0300

    commit changes

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsnycTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsnycTaskTest.java
@@ -1,21 +1,29 @@
 package com.udacity.gradle.builditbigger;
 
 import android.app.Application;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
 import android.text.TextUtils;
+import android.util.Log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
 
 // TODO 54 ) Adding Functional Tests as defined AsnycTaskTest
-public class AsnycTaskTest extends ApplicationTestCase {
+@RunWith(AndroidJUnit4.class)
+public class AsnycTaskTest {
 
-    // TODO 55 ) Defning jokeTestTask as a String
+    // TODO 55 ) Defining jokeTestTask as a String
     String jokeTestTask = null;
 
-    // TODO 56 ) Implementing constructor as AsnycTaskTest
-    public AsnycTaskTest() {
-        super(Application.class);
-    }
+    // TODO 58 ) Defining result as a String
+    String result = null;
 
-    // TODO 57 ) Defining testEndpointsTask method
+    // TODO 56 ) Defining testEndpointsTask method
+    @Test
     public void testEndpointAsyncTask(){
         EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
         endpointsAsyncTask.setListener(new EndpointsAsyncTask.EndpointsTaskListener() {
@@ -25,7 +33,18 @@ public class AsnycTaskTest extends ApplicationTestCase {
             }
         }).execute();
 
-        // TODO 58 ) Successfully retrieving a non-empty string
+
+        try {
+            result = endpointsAsyncTask.get();
+            Log.d("AsnycTaskTest", "Retrieved a non-empty string successfully: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // TODO 57 ) Successfully retrieving a non-empty string
         assertFalse(TextUtils.isEmpty(jokeTestTask));
+
+        // TODO 59 ) Successfully retrieving a non-empty string as a Result
+        assertNotNull(result);
     }
 }
